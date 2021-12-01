12ab01b3c39f1e9d1394e834649b15c1270e21e5
dit <ditazanelli@gmail.com>
2021-12-01 17:08:04 -0600

    shop functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -103,17 +103,65 @@ var fight = function (enemyName) {
 };
 
 var endGame = function () {
-    // if player is still alive, player wins!
-    if (playerHealth > 0) {
-      window.alert(
-        "Great job, you've survived the game! You now have a score of " +
-          playerMoney +
-          "."
-      );
-    } else {
-      window.alert("You've lost your cyborg in battle.");
-    }
-  };
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert(
+      "Great job, you've survived the game! You now have a score of " +
+        playerMoney +
+        "."
+    );
+  } else {
+    window.alert("You've lost your cyborg in battle.");
+  }
+};
+
+var shop = function () {
+  console.log("Entered the shop");
+  var shopOptionPrompt = window.prompt(
+    "would you like to REFILL your health, UPGRADE ur pack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE'."
+  );
+
+  switch (shopOptionPrompt) {
+    case "refill":
+    case "REFILL":
+      if (playerMoney > 7) {
+        window.alert("Refilling player's health by 20 for 7 dollars.");
+        // increase health and decrease money
+        playerHealth = playerHealth + 20;
+        playerMoney = playerMoney - 7;
+      } else {
+        window.alert("You don't have enough money !");
+      }
+      break;
+
+    case "upgrade":
+    case "UPGRADE":
+      if (playerMoney >= 7) {
+        window.alert("Upgrading player's attack by 6 for $7.");
+
+        // increase attack and decrease money
+        playerAttack = playerAttack + 6;
+        playerMoney = playerMoney - 7;
+      } else {
+        window.alert("You don't have enough money!");
+      }
+      break;
+
+    case "leave":
+    case "LEAVE":
+      window.alert("Exiting shop.");
+
+      // do nothing, so function will end
+      break;
+
+    default:
+      window.alert("Invalid option. Try again.");
+
+      // call shop() again to force player to pick a valid option
+      shop();
+      break;
+  }
+};
 
 var startGame = function () {
   // reset player stats
@@ -138,6 +186,16 @@ var startGame = function () {
 
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
+    } // if we're not at the last enemy in the array
+    if (playerHealth > 0 && i < enemyNames.length - 1) {
+      var storeConfirm = window.confirm(
+        "The fight is over, visit the store before the next round?"
+      );
+
+      // if yes, take them to the store() function
+      if (storeConfirm) {
+        shop();
+      }
     }
     // if player isn't alive, stop the game
     else {
@@ -152,14 +210,12 @@ startGame();
 
 // function to end the entire game
 
-
 // ask player if they'd like to play again
 var playAgainConfirm = window.confirm("Would you like to play again?");
 
 if (playAgainConfirm) {
   // restart the game
   startGame();
-} 
-else {
+} else {
   window.alert("Thank you for playing Cyborg Gladiators! Come back soon!");
 }
