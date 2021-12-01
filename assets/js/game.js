34b42c3f93a372d591e3084e493c59af6c9dce1a
dit <ditazanelli@gmail.com>
2021-12-01 17:28:56 -0600

    randomized values for health and damage

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,6 +15,12 @@ var enemyName = ["grimes", "alita", "donna haraway"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+var randomNumber = function (min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
 console.log(
   playerName,
   playerAttack + " attack, ",
@@ -42,13 +48,15 @@ var fight = function (enemyName) {
       if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. weenie!");
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
     } else if (promptFight === "fight" || promptFight === "FIGHT") {
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+      enemyHealth = Math.max(0, enemyHealth - damage);
+
       console.log(
         playerName +
           " attacked " +
@@ -74,7 +82,9 @@ var fight = function (enemyName) {
       }
 
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+      playerHealth = Math.max(0, playerHealth - damage);
+
       console.log(
         enemyName +
           " attacked " +
@@ -179,7 +189,7 @@ var startGame = function () {
       var pickedEnemyName = enemyName[i];
 
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
 
       // use debugger to pause script from running and check what's going on at that moment in the code
       // debugger;
