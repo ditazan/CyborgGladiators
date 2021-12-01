7b47d52f094d93c700d28c7c27306ee3b89dc25b
dit <ditazanelli@gmail.com>
2021-12-01 14:36:00 -0600

    initial fighting setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,22 +1,49 @@
+// player stats
 var playerName = window.prompt("What is your cyborg's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
+//enemy stats
 var enemyName = "grimes";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-console.log(playerName, playerAttack, playerHealth);
+var promptFight = window.prompt(
+  "Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."
+);
+
+console.log(
+  playerName,
+  playerAttack + " attack, ",
+  playerHealth + " health, " + "$" + playerMoney
+);
 
 function fight() {
   window.alert("the fight has begun");
 }
 
-// beg of fight
+// fight function
 var fight = function () {
   window.alert("Welcome to Cyborg Gladiators!");
-  enemyHealth = enemyHealth - playerAttack;
+  //   enemyHealth = enemyHealth - playerAttack;
 
+  //   console.log(
+  //     playerName +
+  //       " attacked " +
+  //       enemyName +
+  //       ". " +
+  //       enemyName +
+  //       " now has " +
+  //       enemyHealth +
+  //       " health remaining."
+  //   );
+};
+
+// start fight or skip fight
+if (promptFight === "fight" || promptFight === "FIGHT") {
+  fight();
+  enemyHealth = enemyHealth - playerAttack;
   console.log(
     playerName +
       " attacked " +
@@ -25,35 +52,66 @@ var fight = function () {
       enemyName +
       " now has " +
       enemyHealth +
+      " health."
+  );
+
+  //check enemy health
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+  // remove player's health by subtracting the amount set in the enemyAttack variable
+  playerHealth = playerHealth - enemyAttack;
+  console.log(
+    enemyName +
+      " attacked " +
+      playerName +
+      ". " +
+      playerName +
+      " now has " +
+      playerHealth +
       " health remaining."
   );
-};
-
-fight();
-
-//check if player is alive
-if (playerHealth > 0) {
-  console.log("Your player is still alive!");
-} else {
-  console.log("This will run instead.");
-}
-
-
-
-//check enemy health
-if (enemyHealth <= 0) {
-  window.alert(enemyName + " has died!");
-} else {
-  window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  //check if player is alive
+  if (playerHealth > 0) {
+    console.log("Your player is still alive!");
+  } else {
+    console.log("This will run instead.");
+  }
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+    // confirm 
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+  
+    // if yes (true), leave fight
+    if (confirmSkip) {
+      window.alert(playerName + " has decided to skip this fight. weenie !");
+      // subtract money from playerMoney for skipping
+      playerMoney = playerMoney - 2;
+    }
+    // if no (false), ask question again by running fight() again
+    else {
+      fight();
+    }
+  } else {
+  window.alert("not a valid option. try again!");
 }
 
 // enemy attack
-console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+// console.log(
+//   enemyName +
+//     " attacked " +
+//     playerName +
+//     ". " +
+//     playerName +
+//     " now has " +
+//     playerHealth +
+//     " health remaining."
+// );
 
-// check player's health
-if (playerHealth <= 0) {
-  window.alert(playerName + " has died!");
-} 
-else {
-  window.alert(playerName + " still has " + playerHealth + " health left.");
-}
+// // check player's health
+// if (playerHealth <= 0) {
+//   window.alert(playerName + " has died!");
+// } else {
+//   window.alert(playerName + " still has " + playerHealth + " health left.");
+// }
