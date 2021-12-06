1eb4e167427c3f3f15c96aa129db1d7a85c69dd2
dit <ditazanelli@gmail.com>
2021-12-06 16:41:18 -0600

    highscore localstorage

--- assets/js/game.js
+++ assets/js/game.js
@@ -147,9 +147,7 @@ var fight = function (enemy) {
         // leave while() loop since enemy is dead
         break;
       } else {
-        window.alert(
-          enemy.name + " still has " + enemy.health + " health left."
-        );
+        window.alert( enemy.name + " still has " + enemy.health + " health left.");
       }
     } else {
       // remove players's health by subtracting the amount set in the enemyAttack variable
@@ -173,25 +171,40 @@ var fight = function (enemy) {
         // leave while() loop if player is dead
         break;
       } else {
-        window.alert(
-          playerInfo.name + " still has " + playerInfo.health + " health left."
-        );
+        window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
       }
     }
     isPlayerTurn = !isPlayerTurn;
   }
 };
 
-var endGame = function () {
-  // if player is still alive, player wins!
-  if (playerInfo.health > 0) {
-    window.alert(
-      "Great job, you've survived the game! You now have a score of " +
-        playerInfo.money +
-        "."
-    );
-  } else {
-    window.alert("You've lost your cyborg in battle.");
+var endGame = function() {
+  window.alert("The game has now ended. Let's see how you did!");
+
+  // check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0;
+  }
+  // if player has more money than the high score, player has new high score!
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+  } 
+  else {
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+  }
+
+  // ask player if they'd like to play again
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if (playAgainConfirm) {
+    startGame();
+  } 
+  else {
+    window.alert("Thank you for playing Cyborg Gladiators! Come back soon!");
   }
 };
 
